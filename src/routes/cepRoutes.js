6dbe5312ea6f8a33f367a4f98a051254539e4ec6
Arthur <arthurrochadx@gmail.com>
2026-01-12 20:43:09 -0300

    feat(api): getCrawlResults

--- src/routes/cepRoutes.js
+++ src/routes/cepRoutes.js
@@ -4,5 +4,6 @@ const crawlController = require('../controllers/crawlController');
 
 router.post('/crawl', crawlController.createCrawl);
 router.get('/crawl/:crawl_id', crawlController.getCrawlStatus);
+router.get('/crawl/:crawl_id/results', crawlController.getCrawlResults);
 
 module.exports = router
